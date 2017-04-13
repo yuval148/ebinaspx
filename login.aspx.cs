087ffb6819c8931921afee286f2f410d05a2fef5
Yuval Margalit <uv@yuval148.com>
2017-04-13 16:22:10 +0300

    now with POINTS!
    מערכת נקודות מוכנה.
    כמות הנקודות היא כמו התרגילים שמשתמש פתר כפול 10

--- login.aspx.cs
+++ login.aspx.cs
@@ -14,7 +14,7 @@ public partial class login : System.Web.UI.Page
     {
         Session["userName"] = null;
         string tableName = "users";//שם הטבלה
-        string userName, userPass, name, team, xpp;
+        string userName, userPass, name, team;
         string userName2, ID, kita, pic;
         int level = 0;
         opc = 0;
@@ -45,8 +45,6 @@ public partial class login : System.Web.UI.Page
                 name = dt.Rows[0]["name"].ToString();
                 Session["team"] = dt.Rows[0]["team"].ToString();
                 team = dt.Rows[0]["team"].ToString();
-                Session["xpp"] = dt.Rows[0]["xpp"].ToString();
-                xpp = dt.Rows[0]["xpp"].ToString();
                 Session["ID"] = dt.Rows[0]["ID"].ToString();
                 ID = dt.Rows[0]["ID"].ToString();
                 Session["kita"] = dt.Rows[0]["kita"].ToString();
@@ -54,10 +52,24 @@ public partial class login : System.Web.UI.Page
                 Session["pic"] = dt.Rows[0]["pic"].ToString();
                 pic = dt.Rows[0]["pic"].ToString();
 
-                Session["level"] = xpstuf.level(Int32.Parse(xpp));
-                level = xpstuf.level(Int32.Parse(xpp));
+                //פעולת xpp
+                //חשב את כל הcou מהטבלה האישית והשם בXPP
+                string sql1 = "SELECT * FROM ID" + ID + ";";
+                DataTable dtid = MyAdoHelper.ExecuteDataTable(fileName, sql1);
+                int xpp1=0;
+                for(int i=0;i<dtid.Rows.Count;i++)
+                    {
+                    xpp1 += int.Parse(dtid.Rows[i]["cou"].ToString());
+                    }
+                xpp1 = xpp1 * 10;
+                string sql2 = "UPDATE users SET xpp='" + xpp1.ToString() +"' WHERE userName='"+userName+"' AND ID='"+ID+"';" ;
+                MyAdoHelper.DoQuery(fileName, sql2);
+                //סוף הפעולה
+                Session["xpp"] = xpp1.ToString();
+                Session["level"] = xpstuf.level(xpp1);
+                level = xpstuf.level(xpp1);
                 opc = 100;
-                Session["ErrLogin"] = " שלום "+ name + " מקבוצת "+ team + " יש לך " + xpp + " נקודות " +"  ואתה בשלב " + level.ToString();
+                Session["ErrLogin"] = " שלום " + name + " מקבוצת " + team + " יש לך " + xpp1.ToString() + " נקודות " + "  ואתה בשלב " + level.ToString();
                 Response.Redirect("home.aspx");
             }
             else if (userName == "yuval" && userPass == "1234")
