62eb184801ee51d07da323efe7ce279f21c49559
Yuval Margalit <uv@yuval148.com>
2017-04-24 22:15:19 +0300

    כסתח קטן
    סליחה.
    003.009.005

--- homeT.aspx.cs
+++ homeT.aspx.cs
@@ -9,29 +9,15 @@ using System.Data;
 using System.Web.Services;
 
 
-public partial class home : System.Web.UI.Page
+public partial class homeT : System.Web.UI.Page
 {
-    public string json = "";
     protected void Page_Load(object sender, EventArgs e)
     {
         if (Session["userNameT"] == null)
         {
             Response.Redirect("loginT.aspx");
         }
-        DataTable dt;
-        string sql = "";
-        string fileName = "db1.mdb"; //שם המסד
-        sql = "select * from users";
-        dt = MyAdoHelper.ExecuteDataTable(fileName, sql);
-        json = Json(dt);
 
     }
-    public string Json(DataTable table)
-    {
-        {
-            string JSONString = string.Empty;
-            JSONString = Newtonsoft.Json.JsonConvert.SerializeObject(new { stu = table });
-            return JSONString;
-        }
-    }
+ 
 }
